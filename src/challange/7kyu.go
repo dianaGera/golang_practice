779f12ab97f4ad0fe53be85a5c7f70a13bbd325d
Diana Matkava <88348384+dianaGera@users.noreply.github.com>
2023-08-02 22:23:32 +0200

    Update 7kyu.go

--- src/challange/7kyu.go
+++ src/challange/7kyu.go
@@ -1,43 +1,90 @@
-package challange
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - Vowel Count - - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
 
-import (
-	"math"
-)
+// Return the number (count) of vowels in the given string.
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+// The input string will only consist of lower case letters and/or spaces.
 
 
-// Insertion sort
-func SortNumbers(numbers []int) []int {
-	// solution(c(1, 2, 3, 10, 5)) # should return c(1, 2, 3, 5, 10)
-	// solution(NULL)              # should return NULL
-	if cap(numbers) > 0 {
-		for i := range numbers {
-			y := i
-			for y > 0 && numbers[y] < numbers[y-1] {
-				temp := numbers[y]
-				numbers[y] = numbers[y-1]
-				numbers[y-1] = temp
-				y -= 1
-			}
-		}
-		return numbers
-	} else {
-		return []int{} 
+func GetCount(str string) (count int) {
+  count = 0
+  vowels := "aeiou"
+  for _, char := range str {
+    for _, vowel := range vowels {
+      if char == vowel {
+       count++ 
+      }
+    }
+  }
+  return count
+}
+
+
+func GetCount(str string) (count int) {  // (count int) will init var with initial int val as 0
+  for _, c := range str {
+    switch c {
+    case 'a', 'e', 'i', 'o', 'u':
+      count++
+    }
+  }
+  return count
+}
+
+
+func GetCount(strn string) int {
+	count := 0
+	vowels := []string{"a", "e", "i", "o", "u"}  //  create dynamically-sized slice
+	for _, vowel := range vowels {
+		count += strings.Count(strn, vowel)  // str.count(val)
 	}
+	return count
+}
+
+import "regexp"
+
+func GetCount(str string) (count int) {
+	r := regexp.MustCompile("[aeiou]")  // re_pattern = '[aeiou]'
+	vowels := r.FindAllString(str, -1)  // re.find_all(re_pattern, string)
+	return len(vowels)
 }
 
 
 
-func FindNextSquare(sq int64) int64 {
-	// 121 --> 144
-	// 625 --> 676
-	// 114 --> -1 since 114 is not a perfect square
-	i := math.Sqrt(float64(sq))
-	d := math.Floor(i)
-	remainder := i - d
-	if remainder == 0 {
-	  d++
-	  res := int64(math.Pow(d, 2))
-	  return res
-	}
-	return -1
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - Shortest Word - - - - - - - - - - - - - 
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+// Find shortest word in string of words
+
+import (
+  "strings"
+)
+
+func FindShort(s string) (min int) {
+  words := strings.Split(s, " ")
+  for _, word := range words {
+    len_word := len(word)
+    if len_word < min || min == 0 {
+      min = len_word
+    }
   }
+  return 
+}
+
+
+import (
+  "strings"
+)
+
+func FindShort(s string) (min int) {
+  words := strings.Split(s, " ")  // cound also use strings.Fields(s)  split a str into substrings based on white space characters (spaces, tabs, newlines, carriage returns, form feeds, and vertical tabs)
+  for _, word := range words {
+    len_word := len(word)
+    if len_word < min || min == 0 {
+      min = len_word
+    }
+  }
+  return 
+}
+
